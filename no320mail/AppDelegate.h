8f2ff0623fb6a120a551fa115228ae9e8fc99bb5
alfred sang <i5ting@126.com>
2014-07-24 14:02:17 +0800

    Initial Commit

---
+++ no320mail/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  no320mail
+//
+//  Created by alfred sang on 7/24/14.
+//  Copyright (c) 2014 alfred sang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
